49ec31ebdb6b4a0daa29a6a0443cedd69abed018
Bini M Babu <binibabu22@gmail.com>
2024-06-20 11:28:46 +0530

    observable with setintreavl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'observable';
+  timer :any;
+  sampleObservable = new Observable((observer: any) => {
+    let count = 0;
+   this.timer =  setInterval(() => {
+      console.log("Timer running");
+      if (count <= 10) {
+        observer.next(count);
+        count++;
+      }
+      else {
+        observer.complete();
+      }
+    }, 1000)
+  })
+  constructor() {
+    this.sampleObservable.subscribe((result: any) => {
+      console.log(result);
+    },
+      (error) => {
+        console.log("error", error);
+      },
+      () => {
+        console.log("Completed");
+        clearInterval(this.timer);
+      })
+  }
 }
